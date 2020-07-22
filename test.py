e4aeca61877261a892d705ac1b6e5407e995fe24
curiosity806 <46279272+curiosity806@users.noreply.github.com>
2020-07-22 17:37:41 +0900

    풀이 추가

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
-N, M = list(map(int, input().split()))
-cards = list(map(int, input().split()))
+N, M =  list(map(int, input().split()))
+no_listens_or_sees = [] # (듣도 못한 사람) or (보도 못한 사람) 중복을 허용하여 모두 추가
+no_listens_and_sees = [] # (듣도)와 (보도) 둘 다 중복되는 사람을 추가
 
-sum_cards = 0
+for i in range(N + M):
+    no_listens_or_sees.append(input())# 일단 중복을 허용하여 다 추가
 
-for i in range(N - 2):
-    for j in range(i+1, N - 1):
-        for k in range(j+1, N):
-            temp = (cards[i] + cards[j] + cards[k])
-            if M >= temp and ((M - sum_cards) > (M - temp)):
-                sum_cards = cards[i] + cards[j] + cards[k]
-            else:
-                continue
-print(sum_cards)
+no_listens_or_sees.sort() # 정렬
+
+i = 0
+while(i < len(no_listens_or_sees) - 1):
+    if no_listens_or_sees[i] == no_listens_or_sees[i + 1]:
+        no_listens_and_sees.append(no_listens_or_sees[i]) # 중복되는 원소를 발견했을 때는 no_listens_and_sees에 추가 하고
+        i = i + 2 # 인덱스는 + 2
+    else:
+        i = i + 1 # 아닐 때는 인덱스 + 1
+    
+print(len(no_listens_and_sees))
+for no_listen_and_see in no_listens_and_sees:
+    print(no_listen_and_see)
